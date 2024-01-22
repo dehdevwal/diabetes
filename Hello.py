569d8eece023f81dc6eb872bd836f590da946433
dehdevwal <157369916+dehdevwal@users.noreply.github.com>
2024-01-22 12:31:42 +0000

    com2 2

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! dd 👋")
+    st.write("# Welcome to Streamlit! d 👋")
 
     st.sidebar.success("Select a demo above.")
 
